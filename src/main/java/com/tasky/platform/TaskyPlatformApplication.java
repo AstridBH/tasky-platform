da81b8dc1040955bac6552b391cf197bb37a548d
Astrid Boronda Heidinger <astrid.boronda@gmail.com>
2025-07-18 19:37:13 -0500

    first commit

---
+++ src/main/java/com/tasky/platform/TaskyPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.tasky.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskyPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskyPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tasky/platform/TaskyPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tasky.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskyPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
